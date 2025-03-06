b3e0f3bc9abf7c757b3a49734c01be136e5670f2
hoo47 <hoo@channel.io>
2025-03-07 02:59:34 +0900

    feat: TransactionSynchronization 인터페이스에 beforeRollback 메서드 추가

--- src/main/java/io/github/event/transaction/TransactionSynchronization.java
+++ src/main/java/io/github/event/transaction/TransactionSynchronization.java
@@ -1,27 +1,34 @@
 package io.github.event.transaction;
 
 /**
- * 트랜잭션 단계별 콜백을 정의하는 인터페이스
+ * 트랜잭션 동기화를 위한 인터페이스입니다.
+ * 트랜잭션의 각 단계에서 실행될 콜백을 정의합니다.
  */
 public interface TransactionSynchronization {
     
     /**
-     * 트랜잭션 커밋 전에 호출됩니다.
+     * 트랜잭션이 커밋되기 전에 호출됩니다.
      */
     default void beforeCommit() {}
     
     /**
-     * 트랜잭션 커밋 후에 호출됩니다.
+     * 트랜잭션이 커밋된 후에 호출됩니다.
      */
     default void afterCommit() {}
     
     /**
-     * 트랜잭션 롤백 후에 호출됩니다.
+     * 트랜잭션이 롤백되기 전에 호출됩니다.
+     */
+    default void beforeRollback() {}
+    
+    /**
+     * 트랜잭션이 롤백된 후에 호출됩니다.
      */
     default void afterRollback() {}
     
     /**
-     * 트랜잭션 완료 후(커밋 또는 롤백)에 호출됩니다.
+     * 트랜잭션이 완료된 후에 호출됩니다.
+     * 커밋이나 롤백 상관없이 항상 호출됩니다.
      */
     default void afterCompletion() {}
 } 
